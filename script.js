acf4eb9cfaecc0bf5a98ba288507c0eb59324086
Polina Ponezha <gurighan511@gmail.com>
2024-06-02 22:54:08 +0300

    some problems with grid

--- script.js
+++ script.js
@@ -8,44 +8,23 @@ document.getElementById('restart-button').addEventListener('click', function() {
     document.querySelector('.game-screen').style.display = 'none';
 });
 
-function createSnowflake() {
-    const snowflake = document.createElement('div');
-    snowflake.classList.add('snowflake');
-    const size = Math.random() * 10 + 5 + 'px';
-    snowflake.style.width = size;
-    snowflake.style.height = size;
-    snowflake.style.left = Math.random() * window.innerWidth + 'px';
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
-    snowflake.style.animationDelay = Math.random() * 5 + 's';
-    document.getElementById('snowflakes-container').appendChild(snowflake);
 
-    // Удаление снежинки после окончания анимации
-    snowflake.addEventListener('animationend', () => {
-        snowflake.remove();
-    });
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const cells = document.querySelectorAll('.cell');
 
-setInterval(createSnowflake, 200);
-
-window.addEventListener('resize', () => {
-    const snowflakes = document.querySelectorAll('.snowflake');
-    snowflakes.forEach(snowflake => {
-        snowflake.style.left = Math.random() * window.innerWidth + 'px';
+    cells.forEach(cell => {
+        cell.addEventListener('click', () => {
+            const choice = prompt('What do you want to place? Type "cat" or "dog"');
+            if (choice === 'cat' || choice === 'dog') {
+                // Скрываем изображение во всех ячейках
+                cells.forEach(cell => {
+                    cell.querySelector('img').style.display = 'none';
+                });
+                // Показываем изображение только в выбранной ячейке
+                const image = cell.querySelector('img');
+                image.src = `art-material/${choice}.png`;
+                image.style.display = 'block';
+            }
+        });
     });
 });
-
-// Анимация текста в меню
-const text = document.querySelector('.typewriter');
-const textContent = text.textContent;
-text.textContent = '';
-let index = 0;
-
-function animateText() {
-    text.textContent += textContent[index];
-    index++;
-    if (index < textContent.length) {
-        setTimeout(animateText, 50); // Интервал между буквами
-    }
-}
-
-animateText();
